9c6ebd7162d65258800729db36cf40ed80a087f9
Ravibsurya <ravibsurya1994@gmail.com>
2023-07-19 23:02:02 +0530

    All Changes are committed and pushed
    All Changes are committed and pushed

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log("before initialization :" + myFullName);
+myFullName="after initialization : Ravindra Suryavanshi ";
+console.log(myFullName);
+myFullName="after updation  : Ravindra Bhagawan Suryavanshi";
+console.log(myFullName);
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+var myLove="my love : I love only JS";
+console.log(myLove);
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+var firstName = "firstName : Ravindra";
+console.log(firstName);
+var lastName = "lastName : Suryavanshi";
+console.log(lastName);
+var age = "age : 28"
+console.log(age);
+var address = "address : Sangli"
+console.log(address);
+var pincode = "pincode : 416405"
+console.log(pincode);
+var gender = "gender : male"
+console.log(gender);
+var isMarried = "is married : false"
+console.log(isMarried);
---
+++ 02_typeOf.js
@@ -0,0 +1,24 @@
+var bankName;
+console.log("before initialization value is : " + bankName);
+bankName = "after initialization : SBI Bank"
+console.log(bankName);
+console.log("data type = "+ typeof bankName);
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+var marks = "marks = 90%";
+console.log(marks);
+console.log("data type = "+ typeof marks);
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+var isWorking;
+console.log("before initialization : " + isWorking);
+isWorking = "after initialization : true"
+console.log(isWorking);
+isWorking=true
+console.log("data type = "+ typeof isWorking);
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+var totalCount = "value is = Hundred and Seven ";
+console.log(totalCount);
+console.log("data type = "+ typeof totalCount);
+var totalCount = "value is = 107"
+console.log(totalCount);
+totalCount=107
+console.log( "data type = " + typeof totalCount);
