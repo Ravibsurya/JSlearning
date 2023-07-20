14ae31b17e5b87b80dcbb9d071f22f86f1f496a9
Ravibsurya <ravibsurya1994@gmail.com>
2023-07-20 16:19:46 +0530

    All the changes are committed and pushed
    All the changes are committed and pushed

---
+++ 01_swapVariables.js
@@ -0,0 +1,17 @@
+var sweety = "sweety";
+var cutie = "cutie";
+console.log('Before swap','Sweety-',sweety,'Cutie-',cutie);
+var temp = sweety
+sweety= cutie
+cutie = temp
+console.log('After swap','Sweety-',sweety,'Cutie-',cutie);
+console.log("-------------------------------------------------------------");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log('Before swap','num1-',num1,'num2-',num2,'num3-',num3); 
+var temp = num1
+num1=num2
+num2=num3
+num3=temp
+console.log('After swap','num1-',num1,'num2-',num2,'num3-',num3); 
