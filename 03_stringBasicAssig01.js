a78df79c3c3dc53708fe3426fb709a24af8d4686
Ravibsurya <ravibsurya1994@gmail.com>
2023-07-22 23:10:02 +0530

    All the changes are committed an pushed
    All the changes are committed an pushed

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,17 @@
+console.log(`==============================================================`);
+function stringBasic(){
+    var company="Infosys";
+    console.log(`My Dream Company is :- ${company}`);
+}
+stringBasic();
+
+console.log(`===============================================================`);
+var hobby1="Creating Status";
+var hobby2="Listening Music";
+var hobby3="Playing Cricket";
+console.log(`My Hobbies are :- ${hobby1}, ${hobby2}, ${hobby3}`);
+
+console.log(`===============================================================`);
+var greet ="Creating Status,Listening Music,Playing Cricket";
+var lengthGreet= greet.length;
+console.log(`Sum the total number of characters is :- ${lengthGreet}`);
