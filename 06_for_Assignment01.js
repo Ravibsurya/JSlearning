8ca1fce6564e39f1093c997cfe857c5e4686e6cb
Ravibsurya <ravibsurya1994@gmail.com>
2023-07-31 14:31:17 +0530

    All the changes are committed and pushed
    All the changes are committed and pushed

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,43 @@
+console.log("--------- Assignment No.1 --------------");
+var result = "";
+for (let index = 5; index <=15; index++) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 50; index >=40; index--) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 1; index <=29; index=index+2) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 0; index <=18; index=index+2) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 5; index <=50; index=index+5) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 10; index <=100; index=index+10) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
+var result = "";
+for (let index = 100; index >=10; index=index-10) {
+    result = result.concat(index).concat(" "); 
+}
+console.log(result);
+console.log("----------------------------------------");
